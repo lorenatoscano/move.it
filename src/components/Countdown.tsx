689111f4e930425ad837b1f6d29202d7c94fff3e
lorenatoscano <lorenatoscano@ufrn.edu.br>
2021-06-04 15:55:12 -0300

    Adiciona componentes e estilização de barra de experiencia, perfil, desafios completos e countdown

---
+++ src/components/Countdown.tsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react';
+import styles from '../styles/components/Countdown.module.css';
+
+export function Countdown() {
+  const [time, setTime] = useState(25 * 60);
+  const [active, setActive] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+  const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+
+  function startCountdown() {
+    setActive(true);
+
+  }
+
+  // Sempre que o valor de active e time mudar, se estiver ativo e ainda tiver tempo, decrementa time em 1 a cada segundo
+  useEffect(() => {
+    if (active && time > 0) {
+      setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    }
+  }, [active, time]);
+
+  return (
+    <div>
+      <div className={styles.countdownContainer}>
+        <div>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
+        </div>
+        <span>:</span>
+        <div>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
+        </div>
+      </div>
+
+      <button
+        type="button"
+        className={styles.countdownButton}
+        onClick={startCountdown}
+      >
+        Iniciar um ciclo
+      </button>
+    </div>
+
+  );
+}
